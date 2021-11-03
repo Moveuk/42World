1b26d96375c3608082457f18936806ad65fb924c
moveuk <v_donguk@naver.com>
2021-11-03 22:36:03 +0900

    임시 머지

--- world/src/main/java/com/world/domain/main/vo/MemberVO.java
+++ world/src/main/java/com/world/domain/main/vo/MemberVO.java
@@ -1,146 +1,215 @@
 package com.world.domain.main.vo;
 
-import java.sql.Date;
+import java.util.Date;
 
 import org.springframework.web.multipart.MultipartFile;
 
 public class MemberVO {
 
-	private int memberNo;
-	private String email;
-	private String password;
-	private String name;
-	private Date birth;
-	private int gender;
-	private String title;
-	private String subtitle;
-	private String propic;
-	private int photo;
-	private int board;
-	private int video;
+   private int memberNo;
+   private String email;
+   private String password;
+   private String name;
+   private Date birth;
+   private int gender;
+   private String title;
+   private String subtitle;
+   private String propic;
+   private int photo;
+   private int board;
+   private int video;
+   private String phone;
+   
+   private String searchCondition;
+   private String searchKeyword;
+   private MultipartFile uploadFile;
+   
+   private int guestcount;
+   private int friendcount;
+   private int itemcount;
+   private int dotorycount;
+   private int cartcount;
+   private int totalguestcount;
+   
+   
 
-	private String searchCondition;
-	private String searchKeyword;
-	private MultipartFile uploadFile;
 
-	public int getMemberNo() {
-		return memberNo;
-	}
 
-	public void setMemberNo(int memberNo) {
-		this.memberNo = memberNo;
-	}
+   public int getMemberNo() {
+      return memberNo;
+   }
 
-	public String getPassword() {
-		return password;
-	}
+   public void setMemberNo(int memberNo) {
+      this.memberNo = memberNo;
+   }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+   public String getPassword() {
+      return password;
+   }
 
-	public String getEmail() {
-		return email;
-	}
+   public void setPassword(String password) {
+      this.password = password;
+   }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+   public String getEmail() {
+      return email;
+   }
 
-	public String getName() {
-		return name;
-	}
+   public void setEmail(String email) {
+      this.email = email;
+   }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+   public String getName() {
+      return name;
+   }
 
-	public Date getBirth() {
-		return birth;
-	}
+   public void setName(String name) {
+      this.name = name;
+   }
 
-	public void setBirth(Date birth) {
-		this.birth = birth;
-	}
+   public Date getBirth() {
+      return birth;
+   }
 
-	public int getGender() {
-		return gender;
-	}
+   public void setBirth(Date birth) {
+      this.birth = birth;
+   }
 
-	public void setGender(int gender) {
-		this.gender = gender;
-	}
+   public int getGender() {
+      return gender;
+   }
 
-	public String getTitle() {
-		return title;
-	}
+   public void setGender(int gender) {
+      this.gender = gender;
+   }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+   public String getTitle() {
+      return title;
+   }
 
-	public String getSubtitle() {
-		return subtitle;
-	}
+   public void setTitle(String title) {
+      this.title = title;
+   }
 
-	public void setSubtitle(String subtitle) {
-		this.subtitle = subtitle;
-	}
+   public String getSubtitle() {
+      return subtitle;
+   }
 
-	public String getPropic() {
-		return propic;
-	}
+   public void setSubtitle(String subtitle) {
+      this.subtitle = subtitle;
+   }
 
-	public void setPropic(String propic) {
-		this.propic = propic;
-	}
+   public String getPropic() {
+      return propic;
+   }
 
-	public int getPhoto() {
-		return photo;
-	}
+   public void setPropic(String propic) {
+      this.propic = propic;
+   }
 
-	public void setPhoto(int photo) {
-		this.photo = photo;
-	}
+   public int getPhoto() {
+      return photo;
+   }
 
-	public int getBoard() {
-		return board;
-	}
+   public void setPhoto(int photo) {
+      this.photo = photo;
+   }
 
-	public void setBoard(int board) {
-		this.board = board;
-	}
+   public int getBoard() {
+      return board;
+   }
 
-	public int getVideo() {
-		return video;
-	}
+   public void setBoard(int board) {
+      this.board = board;
+   }
 
-	public void setVideo(int video) {
-		this.video = video;
-	}
+   public int getVideo() {
+      return video;
+   }
 
-	public String getSearchCondition() {
-		return searchCondition;
-	}
+   public void setVideo(int video) {
+      this.video = video;
+   }
 
-	public void setSearchCondition(String searchCondition) {
-		this.searchCondition = searchCondition;
-	}
+   public String getSearchCondition() {
+      return searchCondition;
+   }
 
-	public String getSearchKeyword() {
-		return searchKeyword;
-	}
+   public void setSearchCondition(String searchCondition) {
+      this.searchCondition = searchCondition;
+   }
 
-	public void setSearchKeyword(String searchKeyword) {
-		this.searchKeyword = searchKeyword;
-	}
+   public String getSearchKeyword() {
+      return searchKeyword;
+   }
 
-	public MultipartFile getUploadFile() {
-		return uploadFile;
-	}
+   public void setSearchKeyword(String searchKeyword) {
+      this.searchKeyword = searchKeyword;
+   }
 
-	public void setUploadFile(MultipartFile uploadFile) {
-		this.uploadFile = uploadFile;
-	}
+   public MultipartFile getUploadFile() {
+      return uploadFile;
+   }
+
+   public void setUploadFile(MultipartFile uploadFile) {
+      this.uploadFile = uploadFile;
+   }
+   
+   public void setPhone(String Phone) {
+      this.phone = Phone;
+   }
+
+   public String getPhone() {
+      return phone;
+   }
+   
+   
+   public int getGuestcount() {
+      return guestcount;
+   }
+
+   public void setGuestcount(int guestcount) {
+      this.guestcount = guestcount;
+   }
+
+   public int getFriendcount() {
+      return friendcount;
+   }
+
+   public void setFriendcount(int friendcount) {
+      this.friendcount = friendcount;
+   }
+
+   public int getItemcount() {
+      return itemcount;
+   }
+
+   public void setItemcount(int itemcount) {
+      this.itemcount = itemcount;
+   }
+
+   public int getDotorycount() {
+      return dotorycount;
+   }
+
+   public void setDotorycount(int dotorycount) {
+      this.dotorycount = dotorycount;
+   }
+
+   public int getCartcount() {
+      return cartcount;
+   }
+
+   public void setCartcount(int cartcount) {
+      this.cartcount = cartcount;
+   }
+   
+   public int getTotalguestcount() {
+      return totalguestcount;
+   }
+
+   public void setTotalguestcount(int totalguestcount) {
+      this.totalguestcount = totalguestcount;
+   }
 
 }
